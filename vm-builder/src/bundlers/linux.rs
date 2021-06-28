9b5eb627ae07db2100ce99698b5b8a683d53a0ee
Aliaksei Syrel <alex.syrel@gmail.com>
2021-06-28 12:22:38 +0200

    bundle the app on linux

--- vm-builder/src/bundlers/linux.rs
+++ vm-builder/src/bundlers/linux.rs
@@ -13,5 +13,41 @@ impl LinuxBundler {
 }
 
 impl Bundler for LinuxBundler {
-    fn bundle(&self, _configuration: &BuildOptions) {}
+    fn bundle(&self, _configuration: &BuildOptions) {
+        let bundle_location = self.bundle_location(configuration);
+        let app_name = self.app_name(configuration);
+
+        let app_dir = bundle_location.join(&app_name);
+        let binary_dir = app_dir.join("bin");
+
+        if app_dir.exists() {
+            fs::remove_dir_all(&app_dir).unwrap();
+        }
+        fs::create_dir_all(&app_dir).unwrap();
+        fs::create_dir(&binary_dir).unwrap();
+
+        let target_executable_path = binary_dir.join(&self.executable_name(configuration));
+
+        match fs::copy(
+            self.compiled_executable_path(configuration),
+            &target_executable_path,
+        ) {
+            Ok(_) => {}
+            Err(error) => {
+                panic!(
+                    "Could not copy {} to {} due to {}",
+                    self.compiled_executable_path(configuration).display(),
+                    &target_executable_path.display(),
+                    error
+                );
+            }
+        };
+
+        fs_extra::copy_items(
+            &self.compiled_libraries(configuration),
+            binary_dir,
+            &fs_extra::dir::CopyOptions::new(),
+        )
+            .unwrap();
+    }
 }
