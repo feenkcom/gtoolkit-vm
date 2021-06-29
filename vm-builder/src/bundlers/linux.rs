8f3210606c0c89422bd5a5d9136875b5d783ff34
Aliaksei Syrel <alex.syrel@gmail.com>
2021-06-29 10:40:24 +0200

    bundle in the same folder

--- vm-builder/src/bundlers/linux.rs
+++ vm-builder/src/bundlers/linux.rs
@@ -21,14 +21,12 @@ impl Bundler for LinuxBundler {
 
         let app_dir = bundle_location.join(&app_name);
         let binary_dir = app_dir.join("bin");
-        let library_dir = app_dir.join("lib");
 
         if app_dir.exists() {
             fs::remove_dir_all(&app_dir).unwrap();
         }
         fs::create_dir_all(&app_dir).unwrap();
         fs::create_dir(&binary_dir).unwrap();
-        fs::create_dir(&library_dir).unwrap();
 
         let target_executable_path = binary_dir.join(&self.executable_name(configuration));
 
@@ -49,13 +47,13 @@ impl Bundler for LinuxBundler {
 
         fs_extra::copy_items(
             &self.compiled_libraries(configuration),
-            library_dir,
+            &binary_dir,
             &fs_extra::dir::CopyOptions::new(),
         ).unwrap();
 
         Command::new("patchelf")
             .arg("--set-rpath")
-            .arg("$ORIGIN/../lib")
+            .arg("$ORIGIN")
             .arg(&target_executable_path)
             .status()
             .unwrap();
