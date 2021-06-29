ee0f3d5c4d136bfbf8abf49b470a724ee8afc960
Aliaksei Syrel <alex.syrel@gmail.com>
2021-06-29 10:35:22 +0200

    add runpath to the executable on linux

--- vm-builder/src/bundlers/linux.rs
+++ vm-builder/src/bundlers/linux.rs
@@ -3,6 +3,8 @@ use crate::BuildOptions;
 use std::fs;
 use std::fs::File;
 use std::path::{Path, PathBuf};
+use std::process::Command;
+use std::env::args;
 
 pub struct LinuxBundler {}
 
@@ -19,12 +21,14 @@ impl Bundler for LinuxBundler {
 
         let app_dir = bundle_location.join(&app_name);
         let binary_dir = app_dir.join("bin");
+        let library_dir = app_dir.join("lib");
 
         if app_dir.exists() {
             fs::remove_dir_all(&app_dir).unwrap();
         }
         fs::create_dir_all(&app_dir).unwrap();
         fs::create_dir(&binary_dir).unwrap();
+        fs::create_dir(&library_dir).unwrap();
 
         let target_executable_path = binary_dir.join(&self.executable_name(configuration));
 
@@ -45,9 +49,15 @@ impl Bundler for LinuxBundler {
 
         fs_extra::copy_items(
             &self.compiled_libraries(configuration),
-            binary_dir,
+            library_dir,
             &fs_extra::dir::CopyOptions::new(),
-        )
+        ).unwrap();
+
+        Command::new("patchelf")
+            .arg("--set-rpath")
+            .arg("$ORIGIN/../lib")
+            .arg(&target_executable_path.display())
+            .status()
             .unwrap();
     }
 }
