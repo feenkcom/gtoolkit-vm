9192c77a77c3e2ac0d95e52d8f1ca9d81b27cb01
Aliaksei Syrel <alex.syrel@gmail.com>
2021-07-11 18:48:40 +0200

    fix the syntax error

--- vm-builder/src/libraries/cmake_library.rs
+++ vm-builder/src/libraries/cmake_library.rs
@@ -124,8 +124,12 @@ impl Library for CMakeLibrary {
         config.define("CMAKE_PREFIX_PATH", &cmake_prefix_path);
 
         match options.target() {
-            Target::X8664appleDarwin => config.define("CMAKE_OSX_ARCHITECTURES", "x86_64"),
-            Target::AArch64appleDarwin => config.define("CMAKE_OSX_ARCHITECTURES", "arm64"),
+            Target::X8664appleDarwin => {
+                config.define("CMAKE_OSX_ARCHITECTURES", "x86_64");
+            }
+            Target::AArch64appleDarwin => {
+                config.define("CMAKE_OSX_ARCHITECTURES", "arm64");
+            }
             Target::X8664pcWindowsMsvc => {}
             Target::X8664UnknownlinuxGNU => {}
         }
