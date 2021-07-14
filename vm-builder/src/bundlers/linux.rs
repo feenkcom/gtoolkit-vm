6be26033242e94ef5e54bbccbc1e2c6af97962a5
Aliaksei Syrel <alex.syrel@gmail.com>
2021-07-14 18:40:25 +0200

    set rpath on linux for both executables and all shared libraries

--- vm-builder/src/bundlers/linux.rs
+++ vm-builder/src/bundlers/linux.rs
@@ -1,13 +1,37 @@
 use crate::bundlers::Bundler;
 use crate::options::BundleOptions;
+use std::error::Error;
 use std::fs;
+use std::path::Path;
 use std::process::Command;
+use user_error::UserFacingError;
 
 pub struct LinuxBundler {}
 
 impl LinuxBundler {
     pub fn new() -> Self {
         Self {}
+    }
+
+    fn library_dir_name(&self) -> &str {
+        "lib"
+    }
+
+    fn set_rpath(&self, binary: impl AsRef<Path>) -> Result<(), Box<dyn Error>> {
+        let binary = binary.as_ref();
+        if !Command::new("patchelf")
+            .arg("--set-rpath")
+            .arg(format!("$ORIGIN/../{}/", self.library_dir_name()))
+            .arg(binary)
+            .status()?
+            .success()
+        {
+            return Err(Box::new(UserFacingError::new(format!(
+                "Failed to set RUNPATH of {}",
+                binary.display(),
+            ))));
+        }
+        Ok(())
     }
 }
 
@@ -19,9 +43,7 @@ impl Bundler for LinuxBundler {
         let app_dir = bundle_location.join(&app_name);
         let binary_dir = app_dir.join("bin");
 
-        let library_dir_name = "lib";
-
-        let library_dir = app_dir.join(library_dir_name);
+        let library_dir = app_dir.join(self.library_dir_name());
 
         if app_dir.exists() {
             fs::remove_dir_all(&app_dir).unwrap();
@@ -36,11 +58,7 @@ impl Bundler for LinuxBundler {
                 binary_dir.join(options.bundled_executable_name(executable));
             match fs::copy(&compiled_executable_path, &bundled_executable_path) {
                 Ok(_) => {
-                    Command::new("patchelf")
-                        .arg("--set-rpath")
-                        .arg(format!("$ORIGIN/../{}", library_dir_name))
-                        .status()
-                        .unwrap();
+                    self.set_rpath(&compiled_executable_path).unwrap();
                 }
                 Err(error) => {
                     panic!(
@@ -53,11 +71,25 @@ impl Bundler for LinuxBundler {
             };
         });
 
-        fs_extra::copy_items(
-            &self.compiled_libraries(options),
-            &library_dir,
-            &fs_extra::dir::CopyOptions::new(),
-        )
-        .unwrap();
+        self.compiled_libraries(options)
+            .iter()
+            .for_each(|compiled_library_path| {
+                let bundled_library_path =
+                    library_dir.join(compiled_library_path.file_name().unwrap());
+
+                match fs::copy(&compiled_library_path, &bundled_library_path) {
+                    Ok(_) => {
+                        self.set_rpath(&bundled_library_path).unwrap();
+                    }
+                    Err(error) => {
+                        panic!(
+                            "Could not copy {} to {} due to {}",
+                            &compiled_library_path.display(),
+                            &bundled_library_path.display(),
+                            error
+                        );
+                    }
+                };
+            });
     }
 }
