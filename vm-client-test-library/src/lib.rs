e21c0eb9a7f2fbcc67e6fda0e85398866a70f46d
Aliaksei Syrel <alex.syrel@gmail.com>
2022-03-16 11:13:55 +0100

    add more vm ffi tests

--- vm-client-test-library/src/lib.rs
+++ vm-client-test-library/src/lib.rs
@@ -1,9 +1,57 @@
+use std::ffi::c_void;
+use std::os::raw::c_char;
+
 #[no_mangle]
-pub fn print_current_thread_name() {
-    let current_thread = std::thread::current();
-    println!(
-        "Current thread name: {:?}, id: {:?}",
-        current_thread.name(),
-        current_thread.id()
-    );
+pub fn pass_and_return_bool(boolean: bool) -> bool {
+    return boolean;
+}
+
+#[no_mangle]
+pub fn pass_and_return_i8(number: i8) -> i8 {
+    return number;
+}
+
+#[no_mangle]
+pub fn pass_and_return_u8(number: u8) -> u8 {
+    return number;
+}
+
+#[no_mangle]
+pub fn pass_and_return_i16(number: i16) -> i16 {
+    return number;
+}
+
+#[no_mangle]
+pub fn pass_and_return_u16(number: u16) -> u16 {
+    return number;
+}
+
+#[no_mangle]
+pub fn pass_and_return_i32(number: i32) -> i32 {
+    return number;
+}
+
+#[no_mangle]
+pub fn pass_and_return_u32(number: u32) -> u32 {
+    return number;
+}
+
+#[no_mangle]
+pub fn pass_and_return_f32(number: f32) -> f32 {
+    return number;
+}
+
+#[no_mangle]
+pub fn pass_and_return_f64(number: f64) -> f64 {
+    return number;
+}
+
+#[no_mangle]
+pub fn pass_and_return_pointer(ptr: *const c_void) -> *const c_void {
+    return ptr;
+}
+
+#[no_mangle]
+pub fn pass_and_return_string(ptr: *const c_char) -> *const c_char {
+    return ptr;
 }
