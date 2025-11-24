5c4453d0136a0d21f6b46524b0c23b448a6c1994
Aliaksei Syrel <alex.syrel@gmail.com>
2025-11-24 18:44:23 +0100

    macos.sh: fail on error, print commands

--- jenkins/macos.sh
+++ jenkins/macos.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-env | cut -d= -f1 | sort
+set -euo pipefail
+set -x
 
 : "${VM_BUILDER_VERSION:?VM_BUILDER_VERSION environment variable is required}"
 : "${FEENK_SIGNER_VERSION:?FEENK_SIGNER_VERSION environment variable is required}"
